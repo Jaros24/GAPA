ac931a67076c0d387411c70b68025e4650930233
Jaros24 <jarosad2@msu.edu>
2023-03-14 11:35:10 -0400

    Robustification of directory mapping - in progress

--- run_sim.sh
+++ run_sim.sh
@@ -8,24 +8,22 @@ automation_dir=/mnt/analysis/e17023/Adam/GADGET2/
 start=`date +%s`
 iterations=0
 
+# mark if debug
+debug=1
+
 # load prerequisites
-cd $attpcroot_dir
-source env_fishtank.sh
+source $attpcroot_dir"env_fishtank.sh"
 module load fairroot/18.6.3
 
 while true; do
 	# modify parameters and rename old h5
-	cd $automation_dir"simInput/"
     echo "Modifying parameters"
-	python3 iter.py
+	python3 $automation_dir"simInput/iter.py"
 
     if [ -f $automation_dir"STOP.csv" ]; then
         # Delete STOP.csv and break loop
         echo "STOP.csv found, stopping Loop"
         rm $automation_dir"STOP.csv"
-        
-        # zip output
-        #zip -r $automation_dir"output.zip" $automation_dir"simOutput/" $automation_dir"simInput/parameters.csv"
         break
     else
         if [ -f $automation_dir"BUILD.csv" ]; then
@@ -34,37 +32,41 @@ while true; do
             rm $automation_dir"BUILD.csv"
 
             # build ATTPCROOT
-            cd $attpcroot_dir"build/"
-            make -j8
+            make -C $automation_dir"build/" -j8
 
             # build ROOT2HDF
-            cd $attpcroot_dir"compiled/ROOT2HDF/build/"
-            make
+            make -C $attpcroot_dir"compiled/ROOT2HDF/build/"
         fi
 
         # copy simulation files
         rm $attpcroot_dir"macro/Simulation/GADGET/Mg20_test_sim.C"
         cp $automation_dir"simInput/Mg20_test_sim.txt" $attpcroot_dir"macro/Simulation/GADGET/Mg20_test_sim.C"
         
-        # run simulation
-        cd $attpcroot_dir"macro/Simulation/GADGET/"
         
-        echo "Mg20_test_sim.C"
-        nohup root -b -l Mg20_test_sim.C &
-        pid1=$!
-        wait $pid1
-        
-        echo "rundigi_sim.C"
-        nohup root -b -l rundigi_sim.C &
-        pid2=$!
-        wait $pid2
+        # run simulation        
+        if [ $debug -eq 1 ]; then
+
+            echo "Mg20_test_sim.C"
+            root -b -l $attpcroot_dir"macro/Simulation/GADGET/Mg20_test_sim.C"
+            echo "rundigi_sim.C"
+            root -b -l $attpcroot_dir"macro/Simulation/GADGET/rundigi_sim.C"
+        else
+            echo "Mg20_test_sim.C"
+            nohup root -b -l $attpcroot_dir"macro/Simulation/GADGET/Mg20_test_sim.C" &
+            pid1=$!
+            wait $pid1
+            
+            echo "rundigi_sim.C"
+            nohup root -b -l $attpcroot_dir"macro/Simulation/GADGET/rundigi_sim.C" &
+            pid2=$!
+            wait $pid2
+        fi
 
         # convert root files to h5
-        cd $attpcroot_dir"compiled/ROOT2HDF/build/"
         echo "Converting root files to h5"
-        ./R2HExe output_digi.root
+        ./$attpcroot_dir"compiled/ROOT2HDF/build/R2HExe" output_digi.root
 
-        mv output.h5 $automation_dir"simOutput/output.h5"
+        mv $attpcroot_dir"compiled/ROOT2HDF/build/output.h5" $automation_dir"simOutput/output.h5"
         ((iterations++))
     fi
 
