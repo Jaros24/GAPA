9815a3074476c95c0e9a6a16f5fda2dc6b30d248
Jaros24 <jarosad2@msu.edu>
2023-12-21 11:20:51 -0500

    add option flags to run_sim.sh, split iter-params into queue-sim and process-sim

--- run_sim.sh
+++ run_sim.sh
@@ -1,13 +1,62 @@
 #!/bin/bash
 
-debug=0 #"n" # set to y to enable debug mode
+debug_log=0 # -l option
+tuning=0 # -t option
+iter_limit=0 # -i option
+var_params=0 # -v option
+reset_params=0 # -d option
+
+# parse command line arguments for options
+while getopts "htvild" option; do
+    case ${option} in
+        h ) # display help
+            echo "Usage: run_sim.sh [-h][-t][-v][-i][-l][-d]"
+            echo "  -t  run simulation in tuning mode"
+            echo "  -v  generate parameters with variation script"
+            echo "  -i  limit number of iterations for testing"
+            echo "  -l  display full logs in terminal"
+            echo "  -d  reset parameter file for debugging"
+            echo "  -h  display help message"
+            echo "See documentation on GitHub for more information"
+            echo "https://github.com/Jaros24/GADGET2"
+            exit 0
+            ;;
+        t ) # run simulation in tuning mode
+            echo "tuning mode enabled"
+            tuning="y"
+            ;;
+        v ) # generate parameters with variation script
+            echo "parameters will be generated with variation script"
+            var_params="y"
+            ;;
+        i ) # limit number of iterations to 1
+            echo "limiting number of iterations to 1"
+            iter_limit="y"
+            ;;
+        l ) # display logs instead of supressing to log file
+            echo "build and simulation logs will be displayed"
+            echo "you will need to input '.q' to exit ROOT after each simulation"
+            debug_log="y"
+            ;;
+        d ) # parameter debug mode
+            echo "parameters.csv will be reset at end of loop"
+            reset_params="y"
+            ;;
+        \? ) # invalid option
+            echo "Invalid option: -$OPTARG" 1>&2
+            exit 1
+            ;;
+
+    esac
+done
+
 
 # set paths for ATTPCROOT and Automation scripts
 automation_dir=$(dirname "$(readlink -f "$0")") # get parent directory of script
 automation_dir=$(readlink -f "$automation_dir" | sed 's:\([^/]\)$:\1/:') # add trailing slash if not present
 rm -f $automation_dir"log.log" # remove old log file if present
 
-# test for ATTPCROOT in same directory as automation scripts
+# test for ATTPCROOT in same directory as automation script
 attpcroot_dir="$automation_dir/../ATTPCROOTv2" # set ATTPCROOT path relative to script location
 attpcroot_dir=$(readlink -f "$attpcroot_dir")
 attpcroot_dir="${attpcroot_dir%/GADGET2/..}"
@@ -22,102 +71,73 @@ if [ ! -d "$attpcroot_dir" ] || [ ! -f "$attpcroot_dir/env_fishtank.sh" ]; then
     fi
 else
     attpcroot_dir=$(readlink -f "$attpcroot_dir" | sed 's:\([^/]\)$:\1/:') # add trailing slash if not present
-    #echo "ATTPCROOT directory found automatically at $attpcroot_dir"n
 fi
 
 source $attpcroot_dir"env_fishtank.sh"
-
-# check for if ATTPCROOT is already built
+# check for if ATTPCROOT is already built, if not build it
 if [ ! -f $attpcroot_dir"build/Makefile" ]; then
     echo "ATTPCROOT not setup, building"
     source $attpcroot_dir"env_fishtank.sh"
     mkdir -p $attpcroot_dir"build"
     cd $attpcroot_dir"build"
     # directories for fairroot and fairsoft are hardcoded, change if needed
-    cmake -DCMAKE_PREFIX_PATH=/mnt/simulations/attpcroot/fair_install_18.6.3/ -DCMAKE_INSTALL_PATH=/mnt/misc/sw/x86_64/all/gnu/gcc/9.3.0/bin/gcc-9.3/ ../ >> $automation_dir"log.log"
-    make install -j8 >> $automation_dir"log.log"
+    if debug_log == "y"; then
+        cmake -DCMAKE_PREFIX_PATH=/mnt/simulations/attpcroot/fair_install_18.6.3/ -DCMAKE_INSTALL_PATH=/mnt/misc/sw/x86_64/all/gnu/gcc/9.3.0/bin/gcc-9.3/ ../
+        make install -j8
+    else
+        cmake -DCMAKE_PREFIX_PATH=/mnt/simulations/attpcroot/fair_install_18.6.3/ -DCMAKE_INSTALL_PATH=/mnt/misc/sw/x86_64/all/gnu/gcc/9.3.0/bin/gcc-9.3/ ../ >> $automation_dir"log.log"
+        make install -j8 >> $automation_dir"log.log"
+    fi
     cd $automation_dir
-
     mkdir -p $attpcroot_dir"macro/Simulation/Charge_Dispersion/data"
 fi
 
-# check for if R2HMain is already built
-#if [ ! -f $attpcroot_dir"compiled/ROOT2HDF/R2HMain/build" ]; then
-#    mkdir -p $attpcroot_dir"compiled/ROOT2HDF/build"
-#    cd $attpcroot_dir"compiled/ROOT2HDF/build"
-#    cmake ../ >> $automation_dir"log.log"
-#    make -j8 >> $automation_dir"log.log"
-#    cd $automation_dir
-#fi
-
-# ask for type of simulation loop to run
-read -p "Is this a tuning simulation? (y/n): " tuning
-
 # convert ipynb to py
-python3 $automation_dir"simInput/nb2py.py" $automation_dir"simInput/iter-params.ipynb"
+python3 $automation_dir"simInput/nb2py.py" $automation_dir"simInput/queue-sim.ipynb"
+python3 $automation_dir"simInput/nb2py.py" $automation_dir"simInput/process-sim.ipynb"
 python3 $automation_dir"simInput/nb2py.py" $automation_dir"simInput/create-params.ipynb"
 python3 $automation_dir"simInput/nb2py.py" $automation_dir"simInput/tuning-params.ipynb"
 python3 $automation_dir"simInput/nb2py.py" $automation_dir"simInput/tuning-view.ipynb"
 python3 $automation_dir"simInput/nb2py.py" $automation_dir"simInput/h5-to-img.ipynb"
 
-if [ $tuning == "y" ]; then
-    echo "Tuning simulation"
-else
-    # echo "Standard simulation"
-    # prompt user for parameters file (generate or use existing)
-    read -p "Generate new parameters? (y/n): " new_params
 
-    if [ $new_params == "y" ]; then
-        python3 $automation_dir"simInput/create-params.py" $automation_dir
-    else 
-        #echo "Using existing parameters.csv"
-        if [ $debug == "y" ]; then
-            echo "using debug parameters"
-            cp $automation_dir"simInput/debug-parameters.csv" $automation_dir"simInput/parameters.csv"
-        fi
-    fi
-
-    # test for parameters.csv
-    if [ ! -f $automation_dir"simInput/parameters.csv" ]; then
-        echo "parameters.csv not found"
-        exit 1
-    fi
+if var_params == "y"; then # run create-params.py if needed
+    echo "Parameter variation script"
+    python3 $automation_dir"simInput/create-params.py" $automation_dir
 fi
 
+if [ ! -f $automation_dir"simInput/parameters.csv" ]; then # test for parameters.csv
+    echo "parameters.csv not found in simInput"
+    exit 1
+fi
+
+if [ $reset_params == "y" ]; then # backup parameters.csv for debugging
+    cp $automation_dir"simInput/parameters.csv" $automation_dir"simInput/queue/parameters.csv"
+fi
 
 # load prerequisites for ATTPCROOT
-#echo "Loading prerequisites"
 source $attpcroot_dir"env_fishtank.sh"
 module load fairroot/18.6.3
 
-# send updated R2HMain.cc and R2HMain.hh to ATTPCROOT (have fix for trace and event data not present in stock version)
-#cp -f $automation_dir"simInput/templates/R2HMain.cc" $attpcroot_dir"compiled/ROOT2HDF/R2HMain.cc"
-#cp -f $automation_dir"simInput/templates/R2HMain.hh" $attpcroot_dir"compiled/ROOT2HDF/R2HMain.hh"
-
 # start timer
-#echo "Starting simulation loop"
 start=`date +%s`
 iterations=0
 
-# run simulation loop
+# SIMULATION LOOP 
 while true; do
     # tuning if needed
-    if [ $tuning == "y" ]; then
-        # run tuning simulation
-        #echo "Tuning simulation"
+    if [ $tuning == "y" ]; then # tuning mode
         python3 $automation_dir"simInput/tuning-params.py" $automation_dir $attpcroot_dir $iterations
         python3 $automation_dir"simInput/tuning-view.py"
     fi
-
-	# modify parameters and rename old h5
-    #echo "Modifying parameters"
-	python3 $automation_dir"simInput/iter-params.py" $automation_dir $attpcroot_dir 2> $automation_dir"log.log"
+    
+	# queue new simulation parameters or break loop
+	python3 $automation_dir"simInput/queue-sim.py" $automation_dir $attpcroot_dir
+    
     if [ -f $automation_dir"STOP.csv" ]; then
         # Delete STOP.csv and break loop
-        #echo "STOP.csv found, stopping Loop"
         rm $automation_dir"STOP.csv"
         break
-
     else
         echo -n "Running simulation"
         # MOVE QUEUED FILES TO SIMULATION FOLDER
@@ -126,18 +146,22 @@ while true; do
         mv -f $automation_dir"simInput/queue/GADGET.sim.par" $attpcroot_dir"parameters/GADGET.sim.par"
         mv -f $automation_dir"simInput/queue/AtTPC20MgDecay_pag.cxx" $attpcroot_dir"AtGenerators/AtTPC20MgDecay_pag.cxx"
         mv -f $automation_dir"simInput/queue/AtPulseGADGET.h" $attpcroot_dir"AtDigitization/AtPulseGADGET.h"
-        # Add more files as implimented in same format
         
         # check if ATTPCROOT needs to be rebuilt
         if [ -f $automation_dir"BUILD.csv" ]; then
-            echo -ne "\r\e[0KRebuilding ATTPCROOT"
-            rm $automation_dir"BUILD.csv"
-            make -C $attpcroot_dir"build/" -j8 &>> $automation_dir"log.log"
-            # make -C $attpcroot_dir"compiled/ROOT2HDF/build/" >> $automation_dir"log.log"
+            if [ $debug_log == "y" ]; then
+                echo "ATTPCROOT needs to be rebuilt"
+                rm $automation_dir"BUILD.csv"
+                make -C $attpcroot_dir"build/" -j8
+            else
+                echo -ne "\r\e[0KATTPCROOT needs to be rebuilt"
+                rm $automation_dir"BUILD.csv"
+                make -C $attpcroot_dir"build/" -j8 &>> $automation_dir"log.log"
+            fi
         fi
 
         # run simulation and digitization
-        if [ $debug == "y" ]; then # debug mode, display output in terminal
+        if [ $debug_log == "y" ]; then # display output in terminal for debugging
             cd $attpcroot_dir"macro/Simulation/Charge_Dispersion/"
             echo 'Mg20_test_sim.C'
             root -l Mg20_test_sim_pag.C 
@@ -158,8 +182,18 @@ while true; do
         fi
         
         mv $attpcroot_dir"macro/Simulation/Charge_Dispersion/data/output.h5" $automation_dir"simOutput/output.h5"
-        echo -ne '\r\e[0K'
+
+        # run processing script
+        echo -ne '\r\e[0Kprocessing simulation'
+        python3 $automation_dir"simInput/process-sim.py" $automation_dir $attpcroot_dir
+
+        echo -ne '\r\e[0K' # clear line in terminal for next iteration
         ((iterations++))
+
+        # break loop if iteration limit is reached
+        if [ $iter_limit == "y" ]; then
+            break
+        fi
     fi
 done
 
@@ -177,12 +211,16 @@ rm -f $automation_dir"nohup.out"
 # copy parameters.csv to simOutput
 cp -f $automation_dir"simInput/parameters.csv" $automation_dir"simOutput/parameters.csv"
 
+# move queued parameters.csv to simInput if it exists (for -d option)
+if [ -f $automation_dir"simInput/queue/parameters.csv" ]; then
+    mv -f $automation_dir"simInput/queue/parameters.csv" $automation_dir"simInput/parameters.csv"
+fi
+
 # convert h5 files into images
-# echo "Producing images from h5 files"
 python3 $automation_dir"simInput/h5-to-img.py" $automation_dir
 rm -f $automation_dir"simInput/h5-to-img.py"
 
-# zip simOutput, named with date and time
+# zip simOutput folder
 cd $automation_dir"simOutput/"
 zip -r output.zip * >> $automation_dir"log.log"
 cd $automation_dir
